623281f694324210694f572873445cfd52ee107d
Adamyuyue <67887367+Adamyuyue@users.noreply.github.com>
2023-11-01 22:44:01 -0500

    first commit

---
+++ backend/functions/database.py
@@ -0,0 +1,71 @@
+import json
+import random
+
+# Get recent messages
+def get_recent_messages():
+    
+    # Define the file name and learn instruction
+    file_name = "stored_data.json"
+    learn_instruction = {
+        "role": "system",
+        "content": "No need to say you don't have feelings and You are a AI. You are an expert of symptoms of cancer patient and work as a consultant for patient. Ask short questions that are relavent to the patient concern to summarize their symptoms. Keep your answers to under 30 words."
+    }
+    
+    # Initialize messages
+    messages = []
+    
+    # Add a random element
+    # x = random.uniform(0, 1)
+    # if x < 0.5:
+    #     learn_instruction["content"] = learn_instruction["content"] + " Your response will include some dry humour."
+    # else:
+    #     learn_instruction["content"] = learn_instruction["content"] + " Your response will include a rather challenging question."
+    
+    # Append instruction to message
+    messages.append(learn_instruction)
+    
+    # Get last messages
+    try:
+        with open(file_name) as user_file:
+            data = json.load(user_file)
+            
+            #Append last 5 items of data
+            if data:
+                if len(data) < 5:
+                    for item in data:
+                        messages.append(item)
+                else:
+                    for item in data[-5:]:
+                        messages.append(item)
+    except Exception as e:
+        print(e)
+        pass
+
+    # Return
+    return messages
+
+
+# Store Messages
+def store_messages(request_message, response_message):
+    
+    # Define the file name
+    file_name = "stored_data.json"
+    
+    # Get recent messages
+    messages = get_recent_messages()[1:]
+    
+    # Add messages to data
+    user_message = {"role": "user", "content": request_message}
+    assistant_message = {"role": "assistant", "content": response_message}
+    messages.append(user_message)
+    messages.append(assistant_message)
+    
+    # Save the updated file
+    with open(file_name, 'w') as f:
+        json.dump(messages, f)
+
+
+# Reset messages
+def reset_messages():
+    # Overwrite current file with nothing
+    open("stored_data.json", "w")
